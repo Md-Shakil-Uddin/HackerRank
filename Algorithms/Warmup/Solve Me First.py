327571926697f2184b18dfa824ae975a4dae6699
Md. Shakil Uddin <104622296+Md-Shakil-Uddin@users.noreply.github.com>
2022-07-11 02:49:55 +0600

    Add files via upload

---
+++ Algorithms/Warmup/Solve Me First.py
@@ -0,0 +1,14 @@
+# solveMeFirst has the following parameters:
+# int a: the first value
+# int b: the second value
+# Returns:
+# int: the sum of a and b
+
+num1 = int(input())
+num2 = int(input())
+
+def solveMeFirst(a,b):
+    # Write your code here
+    return a+b
+print(solveMeFirst(num1,num2))
+
